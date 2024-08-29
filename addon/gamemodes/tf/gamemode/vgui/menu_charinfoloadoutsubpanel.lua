91e005fba90dece02385ab11cd02eec35fdb039a
Techy <33694155+siobhan-saoirse@users.noreply.github.com>
2024-08-29 17:06:01 -0400

    Update menu_charinfoloadoutsubpanel.lua

--- addon/gamemodes/tf/gamemode/vgui/menu_charinfoloadoutsubpanel.lua
+++ addon/gamemodes/tf/gamemode/vgui/menu_charinfoloadoutsubpanel.lua
@@ -380,25 +380,7 @@ function PANEL:PerformLayout()
 
 		for name, wep in pairs(tf_items.Items) do
 			if istable(wep) then
-				if wep.id == tonumber(loadout[4]) and wep.model_player_per_class then
-
-					t:AddModel(2,wep.model_player_per_class[oldclass],{
-						Parent = 1,
-					})
-					
-				elseif wep.id == tonumber(loadout[5]) and wep.model_player_per_class then
-
-					t:AddModel(2,wep.model_player_per_class[oldclass],{
-						Parent = 1,
-					})
-					
-				elseif wep.id == tonumber(loadout[6]) and wep.model_player_per_class then
-
-					t:AddModel(2,wep.model_player_per_class[oldclass],{
-						Parent = 1,
-					})
-					
-				end
+				local oldclass2 = oldclass
 				if (oldclass == "spy" or oldclass == "demoman") then
 					if wep.id == tonumber(loadout[2]) then
 
