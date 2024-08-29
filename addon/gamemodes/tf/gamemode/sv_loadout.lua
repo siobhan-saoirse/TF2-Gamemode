bb513212f9147d3263b58f1596ec077052e02abc
Techy <33694155+siobhan-saoirse@users.noreply.github.com>
2024-08-29 17:19:04 -0400

    Update sv_loadout.lua

--- addon/gamemodes/tf/gamemode/sv_loadout.lua
+++ addon/gamemodes/tf/gamemode/sv_loadout.lua
@@ -3,8 +3,8 @@ local meta = FindMetaTable("Player")
 function meta:GiveLoadout()
     local convar = "loadout_" .. self:GetPlayerClass()
     local split = string.Split(self:GetInfo(convar, "-1,-1,-1,-1,-1"), ",")
-    if #split ~= 5 then
-        split = {-1, -1, -1, -1, -1}
+    if #split ~= 6 then
+        split = {-1, -1, -1, -1, -1, -1}
     end
 
     for type, id in pairs(split) do
