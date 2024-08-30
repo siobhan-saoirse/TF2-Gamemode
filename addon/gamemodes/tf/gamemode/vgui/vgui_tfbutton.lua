32f9803b2328bef93aba6111eadebf124e39dfe9
Techy <33694155+siobhan-saoirse@users.noreply.github.com>
2024-08-30 12:29:21 -0400

    change hover tfbutton color to tforange

--- addon/gamemodes/tf/gamemode/vgui/vgui_tfbutton.lua
+++ addon/gamemodes/tf/gamemode/vgui/vgui_tfbutton.lua
@@ -25,7 +25,7 @@ function PANEL:Paint()
 		local fc, bc
 		if self.Hover then
 			fc = Colors.TanLight
-			bc = Colors.Orange
+			bc = Color(145,73,59,255)
 		else
 			fc = Colors.TanLight
 			bc = Colors.TanDark
