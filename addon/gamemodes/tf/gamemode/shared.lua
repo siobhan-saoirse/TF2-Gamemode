36d480e25c72345766899a41f5f59c3adba5b157
Techy <33694155+siobhan-saoirse@users.noreply.github.com>
2024-08-30 14:21:09 -0400

    Update shared.lua

--- addon/gamemodes/tf/gamemode/shared.lua
+++ addon/gamemodes/tf/gamemode/shared.lua
@@ -1278,6 +1278,46 @@ net.Receive("botVoiceStart", function()
 end)
 hook.Add("Think", "PlayerStuff", function()	
 	for k,pl in ipairs(player.GetAll()) do
+		local v = pl
+		
+		if (v:Alive()) then
+			if (v:IsHL2()) then		  
+				v:SetViewOffset(Vector(0,0,64 * v:GetModelScale()))
+				v:SetViewOffsetDucked(Vector(0, 0, 28 * v:GetModelScale()))
+			else
+				if (v.playerclass == "Scout") then
+					v:SetViewOffset(Vector(0, 0, 65 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 65 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Soldier") then
+					v:SetViewOffset(Vector(0, 0, 68 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 68 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Pyro") then
+					v:SetViewOffset(Vector(0, 0, 68 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 68 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Demoman") then
+					v:SetViewOffset(Vector(0, 0, 68 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 68 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Heavy") then
+					v:SetViewOffset(Vector(0, 0, 75 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 75 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Engineer") then
+					v:SetViewOffset(Vector(0, 0, 68 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 68 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Medic") then
+					v:SetViewOffset(Vector(0, 0, 75 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 75 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Sniper") then
+					v:SetViewOffset(Vector(0, 0, 75 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 75 * (0.5 * v:GetModelScale())))
+				elseif (v.playerclass == "Spy") then
+					v:SetViewOffset(Vector(0, 0, 75 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 75 * (0.5 * v:GetModelScale())))
+				else
+					v:SetViewOffset(Vector(0, 0, 68 * v:GetModelScale()))
+					v:SetViewOffsetDucked(Vector(0, 0, 48 * v:GetModelScale()))
+				end
+			end
+		end
 		
 		if CLIENT then
 			if (pl:GetPlayerClass() == "heavy") then
