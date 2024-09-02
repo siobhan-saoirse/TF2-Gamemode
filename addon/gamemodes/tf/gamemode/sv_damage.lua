5ecd8f08438ec59c5916bd8670052f68378c7f61
Techy <33694155+siobhan-saoirse@users.noreply.github.com>
2024-09-01 21:16:03 -0400

    Update sv_damage.lua

--- addon/gamemodes/tf/gamemode/sv_damage.lua
+++ addon/gamemodes/tf/gamemode/sv_damage.lua
@@ -627,8 +627,12 @@ function GM:EntityTakeDamage(  ent, dmginfo )
 	if dmginfo:GetDamageType() == bit.bor(DMG_BULLET,DMG_ALWAYSGIB) then
 		dmginfo:SetDamageType(DMG_BULLET)
 	end
-	if (att:GetActiveWeapon():Critical()) then
-		dmginfo:SetDamageType(bit.bor(dmginfo:GetDamageType(),DMG_ACID))
+	if (IsValid(att:GetActiveWeapon())) then
+		if (att:GetActiveWeapon().Critical) then
+			if (att:GetActiveWeapon():Critical()) then
+				dmginfo:SetDamageType(bit.bor(dmginfo:GetDamageType(),DMG_ACID))
+			end
+		end
 	end
 	if (ent:IsPlayer() and attacker:GetClass() == "infected") then
 		ent:EmitSound("Player.HitInternal")
